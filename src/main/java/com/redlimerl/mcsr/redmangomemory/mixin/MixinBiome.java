62586a7f6ac6c13bfd49fca96b4a37936bd4078a
Leyla <git@jan-leila.com>
2022-03-24 05:51:22 -0500

    made use redirect in constructor instead of inject at head in usage

--- src/main/java/com/redlimerl/mcsr/redmangomemory/mixin/MixinBiome.java
+++ src/main/java/com/redlimerl/mcsr/redmangomemory/mixin/MixinBiome.java
@@ -2,13 +2,13 @@ package com.redlimerl.mcsr.redmangomemory.mixin;
 
 import it.unimi.dsi.fastutil.longs.Long2FloatLinkedOpenHashMap;
 import net.minecraft.util.Util;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.biome.Biome;
+import org.lwjgl.system.CallbackI;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.function.Supplier;
 
 /**
  *  For fix vanilla memory leak
@@ -27,22 +27,8 @@ public abstract class MixinBiome {
         return long2FloatLinkedOpenHashMap;
     }));
 
-    @Shadow protected abstract float computeTemperature(BlockPos pos);
-
-    @Inject(method = "getTemperature(Lnet/minecraft/util/math/BlockPos;)F", at = @At("HEAD"), cancellable = true)
-    public void stopLeakPlease(BlockPos blockPos, CallbackInfoReturnable<Float> cir) {
-        long l = blockPos.asLong();
-        Long2FloatLinkedOpenHashMap long2FloatLinkedOpenHashMap = fixTemperatureCache.get();
-        float f = long2FloatLinkedOpenHashMap.get(l);
-        if (!Float.isNaN(f)) {
-            cir.setReturnValue(f);
-            return;
-        }
-        float g = this.computeTemperature(blockPos);
-        if (long2FloatLinkedOpenHashMap.size() == 1024) {
-            long2FloatLinkedOpenHashMap.removeFirstFloat();
-        }
-        long2FloatLinkedOpenHashMap.put(l, g);
-        cir.setReturnValue(g);
+    @Redirect(method = "<init>", at=@At(value = "INVOKE", target = "Ljava/lang/ThreadLocal;withInitial(Ljava/util/function/Supplier;)Ljava/lang/ThreadLocal;"))
+    public ThreadLocal<Long2FloatLinkedOpenHashMap> temperatureCacheReplacer(Supplier<? extends CallbackI.S> supplier){
+        return fixTemperatureCache;
     }
 }
